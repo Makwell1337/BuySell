dd1eadeed341abc7e12c54390d2bc8b402680365
Makwell1337 <38865864+Makwell1337@users.noreply.github.com>
2023-09-28 21:33:07 +0300

    Add files via upload

---
+++ buysell/src/main/java/com/example/buysell/configurations/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.example.buysell.configurations;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+}
